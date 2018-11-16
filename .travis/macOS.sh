c7bbcdf47f37f6e90b7931cfc6310ad83a6517d6
Yani Iliev <yani@iliev.me>
2018-11-16 12:47:07 +0700

    Add Qt to the path in travis

--- .travis/macOS.sh
+++ .travis/macOS.sh
@@ -6,6 +6,11 @@ brew update > /dev/null
 
 brew install ccache qt5
 
+export PATH="/usr/local/opt/qt/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/qt/lib"
+export CPPFLAGS="-I/usr/local/opt/qt/include"
+
 cd ..
+
 qmake
 make -j$(sysctl -n hw.ncpu)
