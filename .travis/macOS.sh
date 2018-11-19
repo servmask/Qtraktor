46cfec5d65240f6c91d082ed4691ffea493c3586
Yani Iliev <yani@iliev.me>
2018-11-19 14:23:29 +0700

    Use Qt Installer Framework to create MacOS Installer

--- .travis/macOS.sh
+++ .travis/macOS.sh
@@ -6,7 +6,10 @@ brew update > /dev/null
 
 brew install qt5
 
-export PATH="/usr/local/opt/qt/bin:$PATH"
+brew tap yani-/homebrew-qtifw
+brew install qt-ifw
+
+export PATH="/usr/local/opt/qt/bin:/usr/local/opt/qt-ifw/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/qt/lib"
 export CPPFLAGS="-I/usr/local/opt/qt/include"
 
@@ -18,4 +21,8 @@ make -j$(sysctl -n hw.ncpu)
 # add dependencies
 macdeployqt Traktor.app
 
-hdiutil create -volname "Traktor" -srcfolder "Traktor.app" -format UDZO "Traktor.dmg"
+mkdir packages/com.servmask.traktor/data
+
+cp Traktor.app packages/com.servmask.traktor/data
+
+binarycreator -c config/config.xml -p packages Traktor
